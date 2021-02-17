954fed18c710428a506c197cf22b765bc0c0a27d
Jennifer <jennifer@Jennifers-MacBook-Air.local>
2021-02-17 13:17:32 -0800

    update comment LORE pipeline

--- jen_2021_02_11_lore_pipeline_a.1/extract_equally_spaced_subseq.py
+++ jen_2021_02_11_lore_pipeline_a.1/extract_equally_spaced_subseq.py
@@ -1,3 +1,16 @@
+#  This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#  
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#  
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#  LORE pipeline.v1
 import lore_pipeline_lib
 import argparse
 parser = argparse.ArgumentParser(description='Extracts out a list of subsequences from the raw sequence in RAWSEQFILE. All subsequences ar eof size SEQSIZE. The start index of each subsequence from the reference sequences starts at START (1-index) and increment by INCREMENT. Output sequence is written to file OUTFILE')
